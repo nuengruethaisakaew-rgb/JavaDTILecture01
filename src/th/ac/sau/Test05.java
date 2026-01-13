e98f6b26c27baf5107b4b2f098781e7082ddbb6d
nuengruethaisakaew-rgb <nuengruethaisakaew@gmail.com>
2026-01-13 16:12:00 +0700

    test1-test5

---
+++ src/th/ac/sau/Test05.java
@@ -0,0 +1,24 @@
+package th.ac.sau;
+
+import java.util.Scanner;
+
+//รับคะแนนจำนวนเต็มทางแป้นพิมพ์ แล้วแสดงผล 50 ขึ้นไปผ่าน ไม่ถึงไม่ผ่าน
+public class Test05 {
+    public static void main(String[] args){
+        int score;
+        String result;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ป้อนคะแนน");
+        score = scanner.nextInt();
+        System.out.println("_____________");
+
+        result = score >= 50 ? "ผ่าน" : "ไม่ผ่าน";
+
+        System.out.print("ผลคะแนนคือ: " + result);
+
+
+
+    }
+}
